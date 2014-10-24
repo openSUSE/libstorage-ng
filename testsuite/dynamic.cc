ba96c2581592f37ff50615d97c9524cd10b4efa9
Arvin Schnell <aschnell@suse.de>
2014-10-24 16:01:44 +0200

    - better module name

--- testsuite/dynamic.cc
+++ testsuite/dynamic.cc
@@ -1,6 +1,6 @@
 
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE dynamic
+#define BOOST_TEST_MODULE libstorage
 
 #include <boost/test/unit_test.hpp>
 
--- testsuite/find-vertex.cc
+++ testsuite/find-vertex.cc
@@ -1,6 +1,6 @@
 
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE find_vertex
+#define BOOST_TEST_MODULE libstorage
 
 #include <boost/test/unit_test.hpp>
 
