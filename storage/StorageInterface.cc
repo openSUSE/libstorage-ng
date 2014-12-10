ce5c29bb8a34d4e1556a09b49ac9d2fc6909f2b8
Arvin Schnell <aschnell@suse.de>
2014-12-10 18:34:46 +0100

    - added function to setup logger

--- storage/StorageInterface.cc
+++ storage/StorageInterface.cc
@@ -1,4 +1,7 @@
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 #include "storage/StorageInterface.h"
 #include "storage/Utils/AppUtil.h"
@@ -6,6 +9,26 @@
 
 namespace storage
 {
+
+    void
+    initDefaultLogger(const string& logdir)
+    {
+	string path(logdir);
+	string file("libstorage");
+
+	if (geteuid())
+	{
+	    struct passwd* pw = getpwuid(geteuid());
+	    if (pw)
+	    {
+		path = pw->pw_dir;
+		file = ".libstorage";
+	    }
+	}
+
+	storage_bgl::createLogger(path, file);
+    }
+
 
     CallbackLogDo logger_do_fnc = storage_bgl::defaultLogDo;
     CallbackLogQuery logger_query_fnc = storage_bgl::defaultLogQuery;
