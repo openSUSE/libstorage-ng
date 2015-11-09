065ae1acb6829fdeef76bac7901dca6604c641c7
Arvin Schnell <aschnell@suse.de>
2015-11-09 16:51:03 +0100

    - set BOOST_TEST_CATCH_SYSTEM_ERRORS=no

--- testsuite/systemcmd.cc
+++ testsuite/systemcmd.cc
@@ -26,6 +26,7 @@ join(const vector<string>& input)
     return boost::join(input, "\n") + "\n";
 }
 
+
 BOOST_AUTO_TEST_CASE(hello_stdout)
 {
     vector<string> stdout = {
@@ -40,18 +41,20 @@ BOOST_AUTO_TEST_CASE(hello_stdout)
     BOOST_CHECK(cmd.retcode() == 0);
 }
 
+
 BOOST_AUTO_TEST_CASE(hello_stderr)
 {
     vector<string> stderr = {
 	"stderr #1: hello",
 	"stderr #2: stderr"
     };
-    
+
     SystemCmd cmd("helpers/echoargs_stderr hello stderr");
-    
+
     BOOST_CHECK_EQUAL(join(cmd.stderr()),join(stderr));
     BOOST_CHECK(cmd.stdout().empty());
 }
+
 
 BOOST_AUTO_TEST_CASE(hello_mixed)
 {
@@ -72,32 +75,33 @@ BOOST_AUTO_TEST_CASE(hello_mixed)
     BOOST_CHECK_EQUAL(join(cmd.stderr()), join(stderr));
 }
 
+
 BOOST_AUTO_TEST_CASE(retcode_42)
 {
-#if BOOST_VERSION >= 1058000
-    setenv("BOOST_TEST_CATCH_SYSTEM_ERRORS", "no", 1);
-
     SystemCmd cmd("helpers/retcode 42");
 
     BOOST_CHECK(cmd.retcode() == 42);
-#endif
 }
+
 
 BOOST_AUTO_TEST_CASE(non_existent_no_throw)
 {
     BOOST_CHECK_NO_THROW({SystemCmd cmd("/bin/wrglbrmpf", SystemCmd::ThrowBehaviour::NoThrow);});
 }
 
+
 BOOST_AUTO_TEST_CASE(non_existent_throw)
 {
     BOOST_CHECK_THROW({SystemCmd cmd("/bin/wrglbrmpf", SystemCmd::ThrowBehaviour::DoThrow);},
-	               CommandNotFoundException);
+		      CommandNotFoundException);
 }
+
 
 BOOST_AUTO_TEST_CASE(segfault_no_throw)
 {
     BOOST_CHECK_NO_THROW({SystemCmd cmd( "helpers/segfaulter", SystemCmd::ThrowBehaviour::NoThrow);});
 }
+
 
 BOOST_AUTO_TEST_CASE(segfault_throw)
 {
@@ -105,14 +109,15 @@ BOOST_AUTO_TEST_CASE(segfault_throw)
 		      SystemCmdException);
 }
 
+
 BOOST_AUTO_TEST_CASE(non_exec_no_throw)
 {
     BOOST_CHECK_NO_THROW({SystemCmd cmd( "/etc/fstab", SystemCmd::ThrowBehaviour::NoThrow);});
 }
+
 
 BOOST_AUTO_TEST_CASE(non_exec_throw)
 {
     BOOST_CHECK_THROW({SystemCmd cmd( "/etc/fstab", SystemCmd::ThrowBehaviour::DoThrow);},
 		      SystemCmdException);
 }
-
