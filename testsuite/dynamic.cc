6f8ac079527178b5fb0a0c8bfd8eda60815bf6d1
Arvin Schnell <aschnell@suse.de>
2015-11-13 17:54:32 +0100

    - added testcases

--- testsuite/dynamic.cc
+++ testsuite/dynamic.cc
@@ -44,4 +44,28 @@ BOOST_AUTO_TEST_CASE(dynamic)
 
     BOOST_CHECK(dynamic_cast<const LvmLv*>(devicegraph.find_device(root->get_sid())));
     BOOST_CHECK(dynamic_cast<const Subdevice*>(devicegraph.find_holder(system->get_sid(), root->get_sid())));
+
+    {
+	Device* tmp = sda;
+
+	BOOST_CHECK(is_disk(tmp));
+	BOOST_CHECK_NO_THROW(to_disk(tmp));
+
+	BOOST_CHECK(!is_partition(tmp));
+	BOOST_CHECK_EXCEPTION(to_partition(tmp), DeviceHasWrongType, [](const DeviceHasWrongType& e) {
+	    return strcmp(e.what(), "device has wrong type") == 0;
+	});
+    }
+
+    {
+	Device* tmp = sda1;
+
+	BOOST_CHECK(is_partition(tmp));
+	BOOST_CHECK_NO_THROW(to_partition(tmp));
+
+	BOOST_CHECK(!is_disk(tmp));
+	BOOST_CHECK_EXCEPTION(to_disk(tmp), DeviceHasWrongType, [](const DeviceHasWrongType& e) {
+	    return strcmp(e.what(), "device has wrong type") == 0;
+	});
+    }
 }
