fb294fa501e6dc5502af3cd92842782e1206e9e4
Arvin Schnell <aschnell@suse.de>
2015-12-10 14:35:13 +0100

    - added options to example probe program

--- examples/probe1.cc
+++ examples/probe1.cc
@@ -1,4 +1,6 @@
 
+#include <stdlib.h>
+#include <getopt.h>
 #include <iostream>
 
 #include "storage/Storage.h"
@@ -10,10 +12,17 @@ using namespace std;
 using namespace storage;
 
 
-int
-main()
+bool display_devicegraph = false;
+bool save_devicegraph = false;
+bool save_mockup = false;
+
+
+void
+doit()
 {
-    Environment environment(true, ProbeMode::STANDARD_WRITE_MOCKUP, TargetMode::DIRECT);
+    Environment environment(true, save_mockup ? ProbeMode::STANDARD_WRITE_MOCKUP : ProbeMode::STANDARD,
+			    TargetMode::DIRECT);
+
     environment.set_mockup_filename("probe1-mockup.xml");
 
     Storage storage(environment);
@@ -23,6 +32,72 @@ main()
     cout << *probed << endl;
 
     probed->check();
-    probed->write_graphviz("probe1.gv");
-    probed->save("probe1-devicegraph.xml");
+
+    if (save_devicegraph)
+	probed->save("probe1-devicegraph.xml");
+
+    if (display_devicegraph)
+    {
+	probed->write_graphviz("probe1.gv");
+	system("dot -Tpng < probe1.gv > probe1.png");
+	system("display probe1.png");
+    }
+}
+
+
+void usage() __attribute__ ((__noreturn__));
+
+void
+usage()
+{
+    cerr << "probe1 [--display-devicegraph] [--save-devicegraph] [--save-mockup]" << endl;
+    exit(EXIT_FAILURE);
+}
+
+
+int
+main(int argc, char **argv)
+{
+    const struct option options[] = {
+	{ "display-devicegraph",	no_argument,	0,	1 },
+	{ "save-devicegraph",		no_argument,	0,	2 },
+	{ "save-mockup",		no_argument,	0,	3 },
+	{ 0, 0, 0, 0 }
+    };
+
+    while (true)
+    {
+	int option_index = 0;
+	int c = getopt_long(argc, argv, "", options, &option_index);
+	if (c == -1)
+	    break;
+
+	if (c == '?')
+	    usage();
+
+	switch (c)
+	{
+	    case 1:
+		display_devicegraph = true;
+		break;
+
+	    case 2:
+		save_devicegraph = true;
+		break;
+
+	    case 3:
+		save_mockup = true;
+		break;
+
+	    default:
+		usage();
+	}
+    }
+
+    if (optind < argc)
+	usage();
+
+    doit();
+
+    exit(EXIT_SUCCESS);
 }
