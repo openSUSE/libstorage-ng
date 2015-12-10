1ca8e253a702a49a7a84a468c742db9fc296cd11
Arvin Schnell <aschnell@suse.de>
2015-12-10 14:16:43 +0100

    - updated test program

--- integration-tests/create-partition.py
+++ integration-tests/create-partition.py
@@ -3,6 +3,7 @@
 # requirements: disk /dev/sdb with msdos partition table
 
 
+from sys import exit
 from storage import *
 
 
@@ -31,10 +32,21 @@ disk = Disk.find(staging, "/dev/sdb")
 
 partition_table = disk.get_partition_table()
 
-partition = partition_table.create_partition("/dev/sdb1", PRIMARY)
+partition_slots = partition_table.get_unused_partition_slots()
 
+good_partition_slot = None
+
+for partition_slot in partition_slots:
+    if partition_slot.primary_slot and partition_slot.primary_possible:
+        good_partition_slot = partition_slot
+        break
+
+if not good_partition_slot:
+    print "no good partition slot found"
+    exit()
+
+partition = partition_table.create_partition(good_partition_slot.name, good_partition_slot.region, PRIMARY)
 partition.set_id(0x83)
-partition.set_region(Region(0, 1000))
 
 print staging
 
