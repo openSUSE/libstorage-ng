a082b483dc2218393e9e6eeb91bbd4ecd8fd97c4
Arvin Schnell <aschnell@suse.de>
2015-12-10 14:18:08 +0100

    - added test programs

---
+++ integration-tests/filesystems/create-btrfs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# requirements: disk /dev/sdb with msdos partition table and partition /dev/sdb1
+
+
+from storage import *
+
+
+environment = Environment(False)
+
+storage = Storage(environment)
+
+staging = storage.get_staging()
+
+print staging
+
+partition = Partition.find(staging, "/dev/sdb1")
+partition.set_id(ID_LINUX)
+
+btrfs = partition.create_filesystem(BTRFS)
+btrfs.set_label("TEST")
+btrfs.add_mountpoint("/test")
+
+print staging
+
+storage.calculate_actiongraph()
+storage.commit()
+
