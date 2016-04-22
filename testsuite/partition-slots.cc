377f74a0af48b1036e61697fdc3b1f790eaacc14
Arvin Schnell <aschnell@suse.de>
2016-04-22 16:04:01 +0200

    - adapted to switch from cylinder to sector

--- testsuite/partition-slots.cc
+++ testsuite/partition-slots.cc
@@ -20,6 +20,8 @@ using namespace storage;
 
 BOOST_AUTO_TEST_CASE(test_msdos1)
 {
+    set_logger(get_stdout_logger());
+
     Environment environment(true, ProbeMode::NONE, TargetMode::DIRECT);
 
     Storage storage(environment);
@@ -34,22 +36,23 @@ BOOST_AUTO_TEST_CASE(test_msdos1)
 
     BOOST_CHECK_EQUAL(slots.size(), 1);
 
-    vector<PartitionSlot>::const_iterator it = slots.begin();
-    BOOST_CHECK_EQUAL(it->region.get_start(), 1);
-    BOOST_CHECK_EQUAL(it->region.get_length(), 99999);
-    BOOST_CHECK_EQUAL(it->nr, 1);
-    BOOST_CHECK_EQUAL(it->name, "/dev/sda1");
-    BOOST_CHECK_EQUAL(it->primary_slot, true);
-    BOOST_CHECK_EQUAL(it->primary_possible, true);
-    BOOST_CHECK_EQUAL(it->extended_slot, true);
-    BOOST_CHECK_EQUAL(it->extended_possible, true);
-    BOOST_CHECK_EQUAL(it->logical_slot, false);
-    BOOST_CHECK_EQUAL(it->logical_possible, false);
+    BOOST_CHECK_EQUAL(slots[0].region.get_start(), 2048);
+    BOOST_CHECK_EQUAL(slots[0].region.get_length(), 1000000 - 2048);
+    BOOST_CHECK_EQUAL(slots[0].nr, 1);
+    BOOST_CHECK_EQUAL(slots[0].name, "/dev/sda1");
+    BOOST_CHECK_EQUAL(slots[0].primary_slot, true);
+    BOOST_CHECK_EQUAL(slots[0].primary_possible, true);
+    BOOST_CHECK_EQUAL(slots[0].extended_slot, true);
+    BOOST_CHECK_EQUAL(slots[0].extended_possible, true);
+    BOOST_CHECK_EQUAL(slots[0].logical_slot, false);
+    BOOST_CHECK_EQUAL(slots[0].logical_possible, false);
 }
 
 
 BOOST_AUTO_TEST_CASE(test_msdos2)
 {
+    set_logger(get_stdout_logger());
+
     Environment environment(true, ProbeMode::NONE, TargetMode::DIRECT);
 
     Storage storage(environment);
@@ -60,59 +63,52 @@ BOOST_AUTO_TEST_CASE(test_msdos2)
 
     PartitionTable* msdos = sda->create_partition_table(PtType::MSDOS);
 
-    // primary partition from 0 to 1000
-    msdos->create_partition("/dev/sda1", Region(0, 1000, 512), PartitionType::PRIMARY);
-
-    // extended partition from 2000 to 5000
-    msdos->create_partition("/dev/sda2", Region(2000, 3000, 512), PartitionType::EXTENDED);
+    msdos->create_partition("/dev/sda1", Region(1 * 2048, 10 * 2048, 512), PartitionType::PRIMARY);
+    msdos->create_partition("/dev/sda2", Region(20 * 2048, 10 * 2048, 512), PartitionType::EXTENDED);
 
     vector<PartitionSlot> slots = msdos->get_unused_partition_slots();
 
     BOOST_CHECK_EQUAL(slots.size(), 3);
 
-    // primary possible from 1000 to 2000
-    vector<PartitionSlot>::const_iterator it = slots.begin();
-    BOOST_CHECK_EQUAL(it->region.get_start(), 1000);
-    BOOST_CHECK_EQUAL(it->region.get_length(), 1000);
-    BOOST_CHECK_EQUAL(it->nr, 3);
-    BOOST_CHECK_EQUAL(it->name, "/dev/sda3");
-    BOOST_CHECK_EQUAL(it->primary_slot, true);
-    BOOST_CHECK_EQUAL(it->primary_possible, true);
-    BOOST_CHECK_EQUAL(it->extended_slot, true);
-    BOOST_CHECK_EQUAL(it->extended_possible, false);
-    BOOST_CHECK_EQUAL(it->logical_slot, false);
-    BOOST_CHECK_EQUAL(it->logical_possible, false);
+    BOOST_CHECK_EQUAL(slots[0].region.get_start(), 11 * 2048);
+    BOOST_CHECK_EQUAL(slots[0].region.get_length(), 9 * 2048);
+    BOOST_CHECK_EQUAL(slots[0].nr, 3);
+    BOOST_CHECK_EQUAL(slots[0].name, "/dev/sda3");
+    BOOST_CHECK_EQUAL(slots[0].primary_slot, true);
+    BOOST_CHECK_EQUAL(slots[0].primary_possible, true);
+    BOOST_CHECK_EQUAL(slots[0].extended_slot, true);
+    BOOST_CHECK_EQUAL(slots[0].extended_possible, false);
+    BOOST_CHECK_EQUAL(slots[0].logical_slot, false);
+    BOOST_CHECK_EQUAL(slots[0].logical_possible, false);
 
-    // primary possible from 5000 to 99999
-    ++it;
-    BOOST_CHECK_EQUAL(it->region.get_start(), 5000);
-    BOOST_CHECK_EQUAL(it->region.get_length(), 95000);
-    BOOST_CHECK_EQUAL(it->nr, 3);
-    BOOST_CHECK_EQUAL(it->name, "/dev/sda3");
-    BOOST_CHECK_EQUAL(it->primary_slot, true);
-    BOOST_CHECK_EQUAL(it->primary_possible, true);
-    BOOST_CHECK_EQUAL(it->extended_slot, true);
-    BOOST_CHECK_EQUAL(it->extended_possible, false);
-    BOOST_CHECK_EQUAL(it->logical_slot, false);
-    BOOST_CHECK_EQUAL(it->logical_possible, false);
+    BOOST_CHECK_EQUAL(slots[1].region.get_start(), 30 * 2048);
+    BOOST_CHECK_EQUAL(slots[1].region.get_length(), 1000000 - 30 * 2048);
+    BOOST_CHECK_EQUAL(slots[1].nr, 3);
+    BOOST_CHECK_EQUAL(slots[1].name, "/dev/sda3");
+    BOOST_CHECK_EQUAL(slots[1].primary_slot, true);
+    BOOST_CHECK_EQUAL(slots[1].primary_possible, true);
+    BOOST_CHECK_EQUAL(slots[1].extended_slot, true);
+    BOOST_CHECK_EQUAL(slots[1].extended_possible, false);
+    BOOST_CHECK_EQUAL(slots[1].logical_slot, false);
+    BOOST_CHECK_EQUAL(slots[1].logical_possible, false);
 
-    // logical possible from 2000 to 4999
-    ++it;
-    BOOST_CHECK_EQUAL(it->region.get_start(), 2000);
-    BOOST_CHECK_EQUAL(it->region.get_length(), 2999);
-    BOOST_CHECK_EQUAL(it->nr, 5);
-    BOOST_CHECK_EQUAL(it->name, "/dev/sda5");
-    BOOST_CHECK_EQUAL(it->primary_slot, false);
-    BOOST_CHECK_EQUAL(it->primary_possible, false);
-    BOOST_CHECK_EQUAL(it->extended_slot, false);
-    BOOST_CHECK_EQUAL(it->extended_possible, false);
-    BOOST_CHECK_EQUAL(it->logical_slot, true);
-    BOOST_CHECK_EQUAL(it->logical_possible, true);
+    BOOST_CHECK_EQUAL(slots[2].region.get_start(), 21 * 2048);
+    BOOST_CHECK_EQUAL(slots[2].region.get_length(), 9 * 2048);
+    BOOST_CHECK_EQUAL(slots[2].nr, 5);
+    BOOST_CHECK_EQUAL(slots[2].name, "/dev/sda5");
+    BOOST_CHECK_EQUAL(slots[2].primary_slot, false);
+    BOOST_CHECK_EQUAL(slots[2].primary_possible, false);
+    BOOST_CHECK_EQUAL(slots[2].extended_slot, false);
+    BOOST_CHECK_EQUAL(slots[2].extended_possible, false);
+    BOOST_CHECK_EQUAL(slots[2].logical_slot, true);
+    BOOST_CHECK_EQUAL(slots[2].logical_possible, true);
 }
 
 
 BOOST_AUTO_TEST_CASE(test_msdos3)
 {
+    set_logger(get_stdout_logger());
+
     Environment environment(true, ProbeMode::NONE, TargetMode::DIRECT);
 
     Storage storage(environment);
@@ -130,22 +126,23 @@ BOOST_AUTO_TEST_CASE(test_msdos3)
 
     BOOST_CHECK_EQUAL(slots.size(), 1);
 
-    vector<PartitionSlot>::const_iterator it = slots.begin();
-    BOOST_CHECK_EQUAL(it->region.get_start(), 1);
-    BOOST_CHECK_EQUAL(it->region.get_length(), 99999);
-    BOOST_CHECK_EQUAL(it->nr, 1);
-    BOOST_CHECK_EQUAL(it->name, "/dev/sda1");
-    BOOST_CHECK_EQUAL(it->primary_slot, true);
-    BOOST_CHECK_EQUAL(it->primary_possible, true);
-    BOOST_CHECK_EQUAL(it->extended_slot, true);
-    BOOST_CHECK_EQUAL(it->extended_possible, true);
-    BOOST_CHECK_EQUAL(it->logical_slot, false);
-    BOOST_CHECK_EQUAL(it->logical_possible, false);
+    BOOST_CHECK_EQUAL(slots[0].region.get_start(), 256);
+    BOOST_CHECK_EQUAL(slots[0].region.get_length(), 20000000 - 256);
+    BOOST_CHECK_EQUAL(slots[0].nr, 1);
+    BOOST_CHECK_EQUAL(slots[0].name, "/dev/sda1");
+    BOOST_CHECK_EQUAL(slots[0].primary_slot, true);
+    BOOST_CHECK_EQUAL(slots[0].primary_possible, true);
+    BOOST_CHECK_EQUAL(slots[0].extended_slot, true);
+    BOOST_CHECK_EQUAL(slots[0].extended_possible, true);
+    BOOST_CHECK_EQUAL(slots[0].logical_slot, false);
+    BOOST_CHECK_EQUAL(slots[0].logical_possible, false);
 }
 
 
 BOOST_AUTO_TEST_CASE(test_gpt1)
 {
+    set_logger(get_stdout_logger());
+
     Environment environment(true, ProbeMode::NONE, TargetMode::DIRECT);
 
     Storage storage(environment);
@@ -156,41 +153,34 @@ BOOST_AUTO_TEST_CASE(test_gpt1)
 
     PartitionTable* gpt = sda->create_partition_table(PtType::GPT);
 
-    // primary partition from 0 to 1000
-    Partition* sda1 = gpt->create_partition("/dev/sda1", Region(0, 1000, 512), PartitionType::PRIMARY);
-    sda1->set_region(Region(0, 1000, 512));
-
-    // primary partition from 2000 to 5000
-    Partition* sda2 = gpt->create_partition("/dev/sda2", Region(2000, 3000, 512), PartitionType::PRIMARY);
-    sda2->set_region(Region(2000, 3000, 512));
+    gpt->create_partition("/dev/sda1", Region(1 * 2048, 10 * 2048, 512), PartitionType::PRIMARY);
+    gpt->create_partition("/dev/sda2", Region(20 * 2048, 30 * 2048, 512), PartitionType::PRIMARY);
 
     vector<PartitionSlot> slots = gpt->get_unused_partition_slots();
 
     BOOST_CHECK_EQUAL(slots.size(), 2);
 
-    // primary possible from 1000 to 2000
-    vector<PartitionSlot>::const_iterator it = slots.begin();
-    BOOST_CHECK_EQUAL(it->region.get_start(), 1000);
-    BOOST_CHECK_EQUAL(it->region.get_length(), 1000);
-    BOOST_CHECK_EQUAL(it->nr, 3);
-    BOOST_CHECK_EQUAL(it->name, "/dev/sda3");
-    BOOST_CHECK_EQUAL(it->primary_slot, true);
-    BOOST_CHECK_EQUAL(it->primary_possible, true);
-    BOOST_CHECK_EQUAL(it->extended_slot, true);
-    BOOST_CHECK_EQUAL(it->extended_possible, false);
-    BOOST_CHECK_EQUAL(it->logical_slot, false);
-    BOOST_CHECK_EQUAL(it->logical_possible, false);
+    BOOST_CHECK_EQUAL(slots[0].region.get_start(), 11 * 2048);
+    BOOST_CHECK_EQUAL(slots[0].region.get_length(), 9 * 2048);
+    BOOST_CHECK_EQUAL(slots[0].nr, 3);
+    BOOST_CHECK_EQUAL(slots[0].name, "/dev/sda3");
+    BOOST_CHECK_EQUAL(slots[0].primary_slot, true);
+    BOOST_CHECK_EQUAL(slots[0].primary_possible, true);
+    BOOST_CHECK_EQUAL(slots[0].extended_slot, true);
+    BOOST_CHECK_EQUAL(slots[0].extended_possible, false);
+    BOOST_CHECK_EQUAL(slots[0].logical_slot, false);
+    BOOST_CHECK_EQUAL(slots[0].logical_possible, false);
 
-    // primary possible from 5000 to 9999
-    ++it;
-    BOOST_CHECK_EQUAL(it->region.get_start(), 5000);
-    BOOST_CHECK_EQUAL(it->region.get_length(), 94966);
-    BOOST_CHECK_EQUAL(it->nr, 3);
-    BOOST_CHECK_EQUAL(it->name, "/dev/sda3");
-    BOOST_CHECK_EQUAL(it->primary_slot, true);
-    BOOST_CHECK_EQUAL(it->primary_possible, true);
-    BOOST_CHECK_EQUAL(it->extended_slot, true);
-    BOOST_CHECK_EQUAL(it->extended_possible, false);
-    BOOST_CHECK_EQUAL(it->logical_slot, false);
-    BOOST_CHECK_EQUAL(it->logical_possible, false);
+    // 33 sectors at the end are used for the secondary gpt header
+
+    BOOST_CHECK_EQUAL(slots[1].region.get_start(), 50 * 2048);
+    BOOST_CHECK_EQUAL(slots[1].region.get_length(), 1000000 - 50 * 2048 - 33);
+    BOOST_CHECK_EQUAL(slots[1].nr, 3);
+    BOOST_CHECK_EQUAL(slots[1].name, "/dev/sda3");
+    BOOST_CHECK_EQUAL(slots[1].primary_slot, true);
+    BOOST_CHECK_EQUAL(slots[1].primary_possible, true);
+    BOOST_CHECK_EQUAL(slots[1].extended_slot, true);
+    BOOST_CHECK_EQUAL(slots[1].extended_possible, false);
+    BOOST_CHECK_EQUAL(slots[1].logical_slot, false);
+    BOOST_CHECK_EQUAL(slots[1].logical_possible, false);
 }
