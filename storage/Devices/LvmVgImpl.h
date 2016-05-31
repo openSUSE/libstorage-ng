e11701f75fa31f4839a368b10a2d2a264954b9d2
Arvin Schnell <aschnell@suse.de>
2016-05-31 11:54:15 +0200

    - added uuid to lvm objects

--- storage/Devices/LvmVgImpl.h
+++ storage/Devices/LvmVgImpl.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2016 SUSE LLC
+ *
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, contact Novell, Inc.
+ *
+ * To contact Novell about this file by physical or electronic mail, you may
+ * find current contact information at www.novell.com.
+ */
+
+
 #ifndef STORAGE_LVM_VG_IMPL_H
 #define STORAGE_LVM_VG_IMPL_H
 
@@ -20,7 +42,7 @@ namespace storage
     public:
 
 	Impl(const string& name)
-	    : Device::Impl(), name(name) {}
+	    : Device::Impl(), name(name), uuid() {}
 
 	Impl(const xmlNode* node);
 
@@ -35,6 +57,9 @@ namespace storage
 	const string& get_name() const { return name; }
 	void set_name(const string& name) { Impl::name = name; }
 
+	const string& get_uuid() const { return uuid; }
+	void set_uuid(const string& uuid) { Impl::uuid = uuid; }
+
 	virtual bool equal(const Device::Impl& rhs) const override;
 	virtual void log_diff(std::ostream& log, const Device::Impl& rhs_base) const override;
 
@@ -45,6 +70,7 @@ namespace storage
     private:
 
 	string name;
+	string uuid;
 
     };
 
