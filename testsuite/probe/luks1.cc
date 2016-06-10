1e62aa96839f926d7b546d2f02d5d05b85f2d18a
Arvin Schnell <aschnell@suse.de>
2016-06-10 12:37:59 +0200

    - added luks probing testcase

---
+++ testsuite/probe/luks1.cc
@@ -0,0 +1,39 @@
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE libstorage
+
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+
+#include "storage/Environment.h"
+#include "storage/Storage.h"
+#include "storage/DevicegraphImpl.h"
+
+#include "testsuite/helpers/TsCmp.h"
+
+
+using namespace std;
+using namespace storage;
+
+
+BOOST_AUTO_TEST_CASE(dependencies)
+{
+    set_logger(get_stdout_logger());
+
+    Environment environment(true, ProbeMode::READ_MOCKUP, TargetMode::DIRECT);
+    environment.set_mockup_filename("luks1-mockup.xml");
+
+    Storage storage(environment);
+
+    const Devicegraph* probed = storage.get_probed();
+
+    probed->check();
+
+    Devicegraph* staging = storage.get_staging();
+
+    staging->load("luks1-devicegraph.xml");
+    staging->check();
+
+    TsCmpDevicegraph cmp(*probed, *staging);
+    BOOST_CHECK_MESSAGE(cmp.ok(), cmp);
+}
