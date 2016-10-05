9bfcc0eaa09e162beb3e8af4acef2f81c5797345
Arvin Schnell <aschnell@suse.de>
2016-10-05 11:01:10 +0200

    - improved example

--- examples/create1.cc
+++ examples/create1.cc
@@ -2,12 +2,12 @@
 
 #include <iostream>
 
+#include "storage/Utils/Logger.h"
+#include "storage/Utils/HumanString.h"
 #include "storage/Environment.h"
 #include "storage/Storage.h"
 #include "storage/Devicegraph.h"
 #include "storage/Actiongraph.h"
-#include "storage/Utils/Logger.h"
-#include "storage/Utils/HumanString.h"
 #include "storage/Devices/Disk.h"
 #include "storage/Devices/PartitionTable.h"
 #include "storage/Devices/Partition.h"
@@ -24,37 +24,41 @@ using namespace storage;
 void
 create(Storage& storage)
 {
-    // Get the stating devicegraph
+    // Get the staging devicegraph.
     Devicegraph* staging = storage.get_staging();
 
-    // Find disk "/dev/sdb"
+    // Find disk "/dev/sdb".
     Disk* sdb = Disk::find_by_name(staging, "/dev/sdb");
 
-    // Create a GPT on "sdb"
+    // Create a GPT on "sdb".
     PartitionTable* gpt = sdb->create_partition_table(PtType::GPT);
 
-    // Create partition "sdb1" with 16 GiB size and set id to LVM
-    unsigned long long block_size = sdb->get_region().get_block_size();
-    unsigned long start = 2048;
-    unsigned long length = 16 * GiB / block_size;
-
-    Partition* sdb1 = gpt->create_partition("/dev/sdb1", Region(start, length, block_size),
+    // Create partition "sdb1" with 16 GiB size and set id to LVM. This is
+    // done by querying free slots (there is only one since there are no
+    // partitions yet) and then setting the size of the region. This method
+    // ensures the partition is properly aligned for best performance.
+    PartitionSlot partition_slot = gpt->get_unused_partition_slots()[0];
+    partition_slot.region.set_length(partition_slot.region.to_blocks(16 * GiB));
+    Partition* sdb1 = gpt->create_partition(partition_slot.name, partition_slot.region,
 					    PartitionType::PRIMARY);
     sdb1->set_id(ID_LVM);
 
-    // Create LVM volume group "test" using "sdb1" as physical volume
-    LvmVg* test = LvmVg::create(staging, "test");
-    test->add_lvm_pv(sdb1);
+    // Create volume group "test" using "sdb1" as physical volume.
+    LvmVg* test_vg = LvmVg::create(staging, "test");
+    test_vg->add_lvm_pv(sdb1);
 
-    // Create logical volume "foo" with ext4 on it
-    LvmLv* foo = test->create_lvm_lv("foo", 8 * GiB);
-    Ext4* ext4 = to_ext4(foo->create_filesystem(FsType::EXT4));
-    ext4->add_mountpoint("/test");
+    // Create logical volume "data" with ext4 on it.
+    LvmLv* data_lv = test_vg->create_lvm_lv("data", 8 * GiB);
+    Ext4* ext4_fs = to_ext4(data_lv->create_filesystem(FsType::EXT4));
+    ext4_fs->add_mountpoint("/test");
 
-    // Create logical volume "bar" with swap on it
-    LvmLv* bar = test->create_lvm_lv("bar", 1 * GiB);
-    Swap* swap = to_swap(bar->create_filesystem(FsType::SWAP));
-    swap->add_mountpoint("swap");
+    // Create logical volume "swap" with swap on it.
+    LvmLv* swap_lv = test_vg->create_lvm_lv("swap", 1 * GiB);
+    Swap* swap_fs = to_swap(swap_lv->create_filesystem(FsType::SWAP));
+    swap_fs->add_mountpoint("swap");
+
+    // Check the staging devicegraph.
+    staging->check();
 }
 
 
@@ -80,18 +84,18 @@ public:
 void
 commit(Storage& storage)
 {
-    // Calculate the actiongraph
+    // Calculate the actiongraph.
     const Actiongraph* actiongraph = storage.calculate_actiongraph();
 
 #if 1
 
-    // Print commit actions
+    // Print commit actions.
     for (const string& text : actiongraph->get_commit_actions_as_strings())
 	cout << text << endl;
 
 #else
 
-    // Commit changes to disk
+    // Commit changes to disk.
     MyCommitCallbacks my_commit_callbacks;
     storage.commit(&my_commit_callbacks);
 
@@ -102,10 +106,11 @@ commit(Storage& storage)
 int
 main()
 {
+    // Log into file (/var/log/libstorage-ng.log).
     set_logger(get_logfile_logger());
 
+    // Create storage object. This also does probing of all devices.
     Environment environment(false, ProbeMode::STANDARD, TargetMode::DIRECT);
-
     Storage storage(environment);
 
     create(storage);
