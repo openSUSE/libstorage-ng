f8bf4ba0423f5d09251597d9f38e4c79cc6e547a
Arvin Schnell <aschnell@suse.de>
2016-10-20 14:02:40 +0200

    - updated unit test to include object for partition table

--- testsuite/relatives.cc
+++ testsuite/relatives.cc
@@ -6,6 +6,7 @@
 
 #include "storage/Utils/HumanString.h"
 #include "storage/Devices/Disk.h"
+#include "storage/Devices/Msdos.h"
 #include "storage/Devices/Gpt.h"
 #include "storage/Devices/Partition.h"
 #include "storage/Devices/LvmVg.h"
@@ -56,19 +57,25 @@ BOOST_AUTO_TEST_CASE(dependencies)
 
     Disk* sda = Disk::create(devicegraph, "/dev/sda");
 
+    Msdos* msdos = Msdos::create(devicegraph);
+    User::create(devicegraph, sda, msdos);
+
     Partition* sda1 = Partition::create(devicegraph, "/dev/sda1", Region(0, 10, 262144), PartitionType::PRIMARY);
-    Subdevice::create(devicegraph, sda, sda1);
+    Subdevice::create(devicegraph, msdos, sda1);
 
     Partition* sda2 = Partition::create(devicegraph, "/dev/sda2", Region(10, 10, 262144), PartitionType::PRIMARY);
-    Subdevice::create(devicegraph, sda, sda2);
+    Subdevice::create(devicegraph, msdos, sda2);
 
     Disk* sdb = Disk::create(devicegraph, "/dev/sdb");
 
+    Gpt* gpt = Gpt::create(devicegraph);
+    User::create(devicegraph, sdb, gpt);
+
     Partition* sdb1 = Partition::create(devicegraph, "/dev/sdb1", Region(0, 10, 262144), PartitionType::PRIMARY);
-    Subdevice::create(devicegraph, sdb, sdb1);
+    Subdevice::create(devicegraph, gpt, sdb1);
 
     Partition* sdb2 = Partition::create(devicegraph, "/dev/sdb2", Region(10, 10, 262144), PartitionType::PRIMARY);
-    Subdevice::create(devicegraph, sdb, sdb2);
+    Subdevice::create(devicegraph, gpt, sdb2);
 
     LvmVg* system = LvmVg::create(devicegraph, "system");
     User::create(devicegraph, sda1, system);
@@ -78,23 +85,27 @@ BOOST_AUTO_TEST_CASE(dependencies)
     LvmLv* system_swap = system->create_lvm_lv("swap", 1 * GiB);
     LvmLv* system_home = system->create_lvm_lv("home", 15 * GiB);
 
-    BOOST_CHECK_EQUAL(devicegraph->num_devices(), 10);
-    BOOST_CHECK_EQUAL(devicegraph->num_holders(), 9);
+    BOOST_CHECK_EQUAL(devicegraph->num_devices(), 12);
+    BOOST_CHECK_EQUAL(devicegraph->num_holders(), 11);
 
     devicegraph->check();
 
-    BOOST_CHECK_EQUAL(sort(sda->get_children()), sort({ sda1, sda2 }));
+    BOOST_CHECK_EQUAL(sort(sda->get_children()), sort({ msdos }));
 
-    BOOST_CHECK_EQUAL(sort(sda1->get_parents()), sort({ sda }));
+    BOOST_CHECK_EQUAL(sort(msdos->get_parents()), sort({ sda }));
+
+    BOOST_CHECK_EQUAL(sort(msdos->get_children()), sort({ sda1, sda2 }));
+
+    BOOST_CHECK_EQUAL(sort(sda1->get_parents()), sort({ msdos }));
 
     BOOST_CHECK_EQUAL(sort(sda1->get_siblings(false)), sort({ sda2 }));
     BOOST_CHECK_EQUAL(sort(system_swap->get_siblings(true)), sort({ system_root, system_home, system_swap }));
 
-    BOOST_CHECK_EQUAL(sort(sda->get_descendants(false)), sort({ sda1, sda2, system, system_root, system_home, system_swap }));
+    BOOST_CHECK_EQUAL(sort(sda->get_descendants(false)), sort({ msdos, sda1, sda2, system, system_root, system_home, system_swap }));
     BOOST_CHECK_EQUAL(sort(system->get_descendants(true)), sort({ system, system_root, system_home, system_swap }));
 
-    BOOST_CHECK_EQUAL(sort(sda1->get_ancestors(false)), sort({ sda }));
-    BOOST_CHECK_EQUAL(sort(system->get_ancestors(true)), sort({ system, sda1, sda, sdb1, sdb }));
+    BOOST_CHECK_EQUAL(sort(sda1->get_ancestors(false)), sort({ msdos, sda }));
+    BOOST_CHECK_EQUAL(sort(system->get_ancestors(true)), sort({ system, sda1, msdos, sda, sdb1, gpt, sdb }));
 
     BOOST_CHECK_EQUAL(sort(sda->get_leaves(false)), sort({ sda2, system_root, system_swap, system_home }));
     BOOST_CHECK_EQUAL(sort(system->get_leaves(false)), sort({ system_root, system_swap, system_home }));
