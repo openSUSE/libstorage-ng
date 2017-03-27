521e278d6ff9f3b83a23a4b70bb9a97004c1908a
Arvin Schnell <aschnell@suse.de>
2017-03-27 14:55:31 +0200

    - create /etc directory when mounting /

--- storage/Filesystems/MountableImpl.cc
+++ storage/Filesystems/MountableImpl.cc
@@ -216,6 +216,13 @@ namespace storage
 	SystemCmd cmd(cmd_line);
 	if (cmd.retcode() != 0)
 	    ST_THROW(Exception("mount failed"));
+
+	if (mountpoint == "/")
+	{
+	    string path = get_storage()->prepend_rootprefix("/etc");
+	    if (access(path.c_str(), R_OK) != 0)
+		createPath(path);
+	}
     }
 
 
