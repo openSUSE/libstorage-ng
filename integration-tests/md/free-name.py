b21d37398e4be2167238d8bfc3b6c933024fe115
Arvin Schnell <aschnell@suse.de>
2017-06-04 16:06:44 +0200

    - added integration test

---
+++ integration-tests/md/free-name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# requirements: none
+
+
+from sys import exit
+from storage import *
+
+
+set_logger(get_logfile_logger())
+
+environment = Environment(False)
+
+storage = Storage(environment)
+storage.probe()
+
+staging = storage.get_staging()
+
+print Md.find_free_name(staging)
+
