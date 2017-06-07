94d25b11fa1104f186dbe79ca4d63c6842d5f2ec
Arvin Schnell <aschnell@suse.de>
2017-06-07 11:01:05 +0200

    - coding style

--- storage/Devices/PartitionTableImpl.h
+++ storage/Devices/PartitionTableImpl.h
@@ -53,7 +53,7 @@ namespace storage
 
 	virtual void probe_pass_1(Devicegraph* probed, SystemInfo& systeminfo) override;
 
-	void check() const override;
+	virtual void check() const override;
 
 	Partition* create_partition(const string& name, const Region& region, PartitionType type);
 
