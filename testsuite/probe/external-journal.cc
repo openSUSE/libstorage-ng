547526e2cfc2995c689364f5fb11e8c9420e4336
Arvin Schnell <aschnell@suse.de>
2017-06-13 23:01:35 +0200

    - added unit test

---
+++ testsuite/probe/external-journal.cc
@@ -0,0 +1,43 @@
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE libstorage
+
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+
+#include "storage/Environment.h"
+#include "storage/Storage.h"
+#include "storage/DevicegraphImpl.h"
+#include "storage/UsedFeatures.h"
+
+#include "testsuite/helpers/TsCmp.h"
+
+
+using namespace std;
+using namespace storage;
+
+
+BOOST_AUTO_TEST_CASE(probe)
+{
+    set_logger(get_stdout_logger());
+
+    Environment environment(true, ProbeMode::READ_MOCKUP, TargetMode::DIRECT);
+    environment.set_mockup_filename("external-journal-mockup.xml");
+
+    Storage storage(environment);
+    storage.probe();
+
+    const Devicegraph* probed = storage.get_probed();
+
+    probed->check();
+
+    Devicegraph* staging = storage.get_staging();
+
+    staging->load("external-journal-devicegraph.xml");
+    staging->check();
+
+    TsCmpDevicegraph cmp(*probed, *staging);
+    BOOST_CHECK_MESSAGE(cmp.ok(), cmp);
+
+    BOOST_CHECK_BITWISE_EQUAL(probed->used_features(), UF_EXT4 | UF_XFS | UF_SWAP);
+}
