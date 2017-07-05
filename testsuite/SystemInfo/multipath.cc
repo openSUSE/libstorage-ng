7c012375b7b7489289c01f5e75c7bb13a3d9bbc3
Arvin Schnell <aschnell@suse.de>
2017-07-05 11:21:11 +0200

    - include multipath with alias in test case

--- testsuite/SystemInfo/multipath.cc
+++ testsuite/SystemInfo/multipath.cc
@@ -55,3 +55,27 @@ BOOST_AUTO_TEST_CASE(parse1)
 
     check(input, output);
 }
+
+
+BOOST_AUTO_TEST_CASE(parse2)
+{
+    vector<string> input = {
+	"36005076305ffc73a00000000000013b4 dm-0 IBM,2107900",
+	"size=1.0G features='1 retain_attached_hw_handler' hwhandler='1 alua' wp=rw",
+	"`-+- policy='service-time 0' prio=50 status=active",
+	"  |- 0:0:0:1085554707 sda 8:0  active ready running",
+	"  `- 0:0:1:1085554707 sdc 8:32 active ready running",
+	"some-alias (36005076305ffc73a00000000000013b5) dm-3 IBM,2107900",
+	"size=1.0G features='1 retain_attached_hw_handler' hwhandler='1 alua' wp=rw",
+	"`-+- policy='service-time 0' prio=50 status=active",
+	"  |- 0:0:0:1085620243 sdb 8:16 active ready running",
+	"  `- 0:0:1:1085620243 sdd 8:48 active ready running"
+    };
+
+    vector<string> output = {
+	"data[36005076305ffc73a00000000000013b4] -> vendor:IBM model:2107900 devices:</dev/sda /dev/sdc>",
+	"data[some-alias] -> vendor:IBM model:2107900 devices:</dev/sdb /dev/sdd>"
+    };
+
+    check(input, output);
+}
