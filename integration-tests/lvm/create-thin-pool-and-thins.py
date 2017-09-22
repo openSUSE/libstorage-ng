53b5458e0eefa1bf5369587613cb6684a305ec82
Arvin Schnell <aschnell@suse.de>
2017-09-22 22:51:21 +0200

    - added integration test

---
+++ integration-tests/lvm/create-thin-pool-and-thins.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# requirements: lvm volume group test without logical volume thin-pool, thin1 and thin2
+
+
+from sys import exit
+from storage import *
+from storageitu import *
+
+
+set_logger(get_logfile_logger())
+
+environment = Environment(False)
+
+storage = Storage(environment)
+storage.probe()
+
+staging = storage.get_staging()
+
+test = LvmVg.find_by_vg_name(staging, "test")
+
+thin_pool = test.create_lvm_lv("thin-pool", LvType_THIN_POOL, 1 * GiB)
+
+thin_pool.create_lvm_lv("thin1", LvType_THIN, 2 * GiB)
+thin_pool.create_lvm_lv("thin2", LvType_THIN, 3 * GiB)
+
+print staging
+
+commit(storage)
+
