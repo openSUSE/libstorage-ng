79ce401c9f9691295efe3369df07003797bd496f
Arvin Schnell <aschnell@suse.de>
2017-09-26 11:07:15 +0200

    - added integration test

---
+++ integration-tests/lvm/delete-thin.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+# requirements: lvm volume group test with thin-pool thin-pool and thin thin1
+
+
+from sys import exit
+from storage import *
+from storageitu import *
+
+
+set_logger(get_logfile_logger())
+
+environment = Environment(False)
+
+storage = Storage(environment)
+storage.probe()
+
+staging = storage.get_staging()
+
+test = LvmVg.find_by_vg_name(staging, "test")
+
+thin_pool = test.get_lvm_lv("thin-pool")
+
+thin1 = thin_pool.get_lvm_lv("thin1")
+
+thin1.remove_descendants()
+staging.remove_device(thin1)
+
+print staging
+
+commit(storage)
+
