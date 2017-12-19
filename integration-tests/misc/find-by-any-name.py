edef32533436915e259a5ef937c6d78a88938cf4
Arvin Schnell <aschnell@suse.de>
2017-12-19 14:15:39 +0100

    - added integration test

---
+++ integration-tests/misc/find-by-any-name.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# requirements: partition /dev/sdb1
+
+
+from sys import exit
+from storage import *
+from storageitu import *
+
+
+set_logger(get_logfile_logger())
+
+environment = Environment(False)
+
+storage = Storage(environment)
+storage.probe()
+
+probed = storage.get_probed()
+
+
+try:
+    BlkDevice.find_by_name(probed, "/dev/block/8:17")
+except DeviceNotFoundByName:
+    print("device not found")
+
+
+blk_device = BlkDevice.find_by_any_name(probed, "/dev/block/8:17")
+print(blk_device.get_name())
+
