d0f225a1124988b23ce013a0e718365288305115
Arvin Schnell <aschnell@suse.de>
2018-03-01 22:03:39 +0100

    - handle missing attribute for pvs

--- testsuite/SystemInfo/pvs.cc
+++ testsuite/SystemInfo/pvs.cc
@@ -54,3 +54,27 @@ BOOST_AUTO_TEST_CASE(parse1)
 
     check(input, output);
 }
+
+
+BOOST_AUTO_TEST_CASE(parse_missing)
+{
+    vector<string> input = {
+	"  {",
+	"      \"report\": [",
+	"          {",
+	"              \"pv\": [",
+	"                  {\"pv_name\":\"/dev/sda1\", \"pv_uuid\":\"smMwqo-zDbE-fa6S-ouy2-MmJN-pxlg-VOjrOX\", \"vg_name\":\"test\", \"vg_uuid\":\"dl6cxn-1P1U-SWjT-Ckhq-viMD-GF9z-SU9XvE\", \"pv_attr\":\"a--\"},",
+	"                  {\"pv_name\":\"[unknown]\", \"pv_uuid\":\"vfbt1o-rEi2-ph1Q-ksMt-qhTw-4lwl-iruKqS\", \"vg_name\":\"test\", \"vg_uuid\":\"dl6cxn-1P1U-SWjT-Ckhq-viMD-GF9z-SU9XvE\", \"pv_attr\":\"a-m\"}",
+	"              ]",
+	"          }",
+	"      ]",
+	"  }"
+    };
+
+    vector<string> output = {
+	"pv:{ pv-name:/dev/sda1 pv-uuid:smMwqo-zDbE-fa6S-ouy2-MmJN-pxlg-VOjrOX vg-name:test vg-uuid:dl6cxn-1P1U-SWjT-Ckhq-viMD-GF9z-SU9XvE }",
+	"pv:{ pv-name:[unknown] pv-uuid:vfbt1o-rEi2-ph1Q-ksMt-qhTw-4lwl-iruKqS vg-name:test vg-uuid:dl6cxn-1P1U-SWjT-Ckhq-viMD-GF9z-SU9XvE missing }"
+    };
+
+    check(input, output);
+}
