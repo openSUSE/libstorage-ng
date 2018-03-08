118f453d18a579a89e9cf62f149cb490ed586f8b
Arvin Schnell <aschnell@suse.de>
2018-03-08 20:39:00 +0100

    - extended unit test

--- testsuite/probe/lvm-errors1.cc
+++ testsuite/probe/lvm-errors1.cc
@@ -11,13 +11,11 @@
 #include "storage/UsedFeatures.h"
 
 #include "testsuite/helpers/TsCmp.h"
+#include "testsuite/helpers/CheckCallbacksRecorder.h"
 
 
 using namespace std;
 using namespace storage;
-
-
-// TODO also check the check functions
 
 
 BOOST_AUTO_TEST_CASE(probe)
@@ -30,12 +28,19 @@ BOOST_AUTO_TEST_CASE(probe)
     Storage storage(environment);
     storage.probe();
 
+    vector<string> messages;
+    CheckCallbacksRecorder check_callbacks_recorder(messages);
+
     const Devicegraph* probed = storage.get_probed();
-    // probed->check();
+    probed->check(&check_callbacks_recorder);
+
+    BOOST_CHECK_EQUAL(messages.size(), 2);
+    BOOST_CHECK_EQUAL(messages[0], "Physical volume lv08LY-exMW-YKNh-GgX7-q0Ja-dW8H-lJ9Ejw is broken.");
+    BOOST_CHECK_EQUAL(messages[1], "Physical volume u6c690-dm2j-IC7t-v8Lg-3Ylk-sUXY-BHsVmY is broken.");
 
     Devicegraph* staging = storage.get_staging();
     staging->load("lvm-errors1-devicegraph.xml");
-    // staging->check();
+    staging->check();
 
     TsCmpDevicegraph cmp(*probed, *staging);
     BOOST_CHECK_MESSAGE(cmp.ok(), cmp);
