1332fbfadfa121cd39340c41ebd2e0e8b8d53aee
Arvin Schnell <aschnell@suse.de>
2018-06-11 21:15:45 +0200

    - extended unit test

--- testsuite/find-vertex.cc
+++ testsuite/find-vertex.cc
@@ -11,8 +11,11 @@
 #include "storage/Environment.h"
 #include "storage/Storage.h"
 #include "storage/Devicegraph.h"
+#include "storage/Utils/Mockup.h"
+#include "storage/Utils/StorageDefines.h"
 
 
+using namespace std;
 using namespace storage;
 
 
@@ -42,4 +45,47 @@ BOOST_AUTO_TEST_CASE(find_vertex)
     BOOST_CHECK(sda->exists_in_devicegraph(devicegraph));
     BOOST_CHECK(!sda->exists_in_probed());
     BOOST_CHECK(sda->exists_in_staging());
+
+    // For testing find_by_any_name some mockup is needed since
+    // find_by_any_name can do a system lookup. find_by_any_name also only
+    // works on the probed and system devicegraph.
+
+    Mockup::set_mode(Mockup::Mode::PLAYBACK);
+
+    Mockup::set_command(UDEVADMBIN " settle --timeout=20", vector<string>({}));
+
+    storage.remove_devicegraph("system");
+    storage.copy_devicegraph("staging", "system");
+
+    const Devicegraph* system = storage.get_system();
+
+    // Looking up a device by the name libstorage-ng knows it does not need
+    // udevadm info calls.
+
+    BOOST_CHECK_EQUAL(BlkDevice::find_by_any_name(system, "/dev/sda1")->get_sid(), sda1->get_sid());
+
+    // Looking up a device by another name needs udevadm info calls.
+
+    Mockup::set_command(UDEVADMBIN " info '/dev/sda'",
+			vector<string>({ "E: MAJOR=8", "E: MINOR=0" }));
+
+    Mockup::set_command(UDEVADMBIN " info '/dev/sda1'",
+			vector<string>({ "E: MAJOR=8", "E: MINOR=1" }));
+
+    Mockup::set_command(UDEVADMBIN " info '/dev/block/8:1'",
+			vector<string>({ "E: MAJOR=8", "E: MINOR=1" }));
+
+    BOOST_CHECK_EQUAL(BlkDevice::find_by_any_name(system, "/dev/block/8:1")->get_sid(), sda1->get_sid());
+
+    Mockup::set_command(UDEVADMBIN " info '/dev/block/8:2'",
+			vector<string>({ "E: MAJOR=8", "E: MINOR=2" }));
+
+    BOOST_CHECK_THROW(BlkDevice::find_by_any_name(system, "/dev/block/8:2"), DeviceNotFound);
+
+    // Looking up a device unknown to udevadm info throws DeviceNotFound.
+
+    Mockup::set_command(UDEVADMBIN " info '/dev/does-not-exist'",
+			RemoteCommand( vector<string>({}), vector<string>({ "Unknown device, [...]" }), 4));
+
+    BOOST_CHECK_THROW(BlkDevice::find_by_any_name(system, "/dev/does-not-exist"), DeviceNotFound);
 }
