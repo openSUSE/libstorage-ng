01d84cfe9424ed70cd56cc2ea9dd042c2efae144
Ladislav Slezák <lslezak@suse.cz>
2018-09-25 10:12:26 +0200

    Added Travis ccache documentation
    Use a different ccache compiler check

--- .travis.sh
+++ .travis.sh
@@ -5,10 +5,41 @@
 # exit on error immediately, print the executed commands
 set -e -x
 
+########
+##
+## Emergency Travis cache cleanup
+##
+## In case the ccache cache gets broken and you need to clear the cache
+##
+##   A) Directly clear the cache at Travis, requires access permissions
+##      (https://travis-ci.org/openSUSE/libstorage-ng/caches)
+##
+##   B) If you cannot clear the cache at Travis (e.g. from a
+##      repository fork) then:
+##        1) Uncomment the block below
+##        2) Push to Github
+##        3) Wait for Travis to execute it
+##        4) Remove the last commit and (force) push again
+##        5) Travis will start a new build with the empty cache and
+##           this script will stay untouched
+####
+## clear the ccache completely
+# ccache -C
+## print the statistics to verify the cache was cleared
+# ccache -s
+## finish the Travis build now, it will save the empty cache
+## for the next build
+# exit 0
+########
+
 # use ccache for faster rebuilds
 export PATH="/usr/lib64/ccache:$PATH"
 # set 2GB cache size
 ccache --set-config=max_size=2.0G
+# use e.g. "gcc -v" to detect the compiler change,
+# the default (slightly faster) "mtime" setting does not work well
+# at Travis because we use a Docker image which is regurarly rebuilt
+ccache "--set-config=compiler_check=%compiler% -v"
 # print the initial ccache statistics
 ccache -s
 
