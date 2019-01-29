729b06d66bc40d199787de0d069ffb02f2b6e9f9
José Iván López González <jlopez@suse.com>
2019-01-29 16:53:06 +0000

    Add test for Flash-only bcache probing

---
+++ testsuite/probe/bcache2.cc
@@ -0,0 +1,42 @@
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE libstorage
+
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+
+#include "storage/Environment.h"
+#include "storage/Storage.h"
+#include "storage/Devicegraph.h"
+#include "storage/UsedFeatures.h"
+
+#include "testsuite/helpers/TsCmp.h"
+
+
+using namespace std;
+using namespace storage;
+
+
+// Test for probing with both types of Bcache devices: Backed and Flash-only.
+BOOST_AUTO_TEST_CASE(probe)
+{
+    set_logger(get_stdout_logger());
+
+    Environment environment(true, ProbeMode::READ_MOCKUP, TargetMode::DIRECT);
+    environment.set_mockup_filename("bcache2-mockup.xml");
+
+    Storage storage(environment);
+    storage.probe();
+
+    const Devicegraph* probed = storage.get_probed();
+    probed->check();
+
+    Devicegraph* staging = storage.get_staging();
+    staging->load("bcache2-devicegraph.xml");
+    staging->check();
+
+    TsCmpDevicegraph cmp(*probed, *staging);
+    BOOST_CHECK_MESSAGE(cmp.ok(), cmp);
+
+    BOOST_CHECK_BITWISE_EQUAL(probed->used_features(), UF_SWAP | UF_NFS | UF_EXT3 | UF_EXT4 | UF_BCACHE);
+}
