8999388663933831ffb4be111eae6230d763e7db
Arvin Schnell <aschnell@suse.de>
2019-03-12 14:37:17 +0100

    - added unit test for bsc #1121129

--- testsuite/lvm-pv-usable-size.cc
+++ testsuite/lvm-pv-usable-size.cc
@@ -76,3 +76,25 @@ BOOST_AUTO_TEST_CASE(disk2)
 
     BOOST_CHECK_EQUAL(lvm_pv->get_usable_size(), sda->get_size() - 2 * MiB);
 }
+
+
+BOOST_AUTO_TEST_CASE(partition2)
+{
+    set_logger(get_stdout_logger());
+
+    Environment environment(true, ProbeMode::NONE, TargetMode::DIRECT);
+
+    Storage storage(environment);
+
+    Devicegraph* staging = storage.get_staging();
+
+    Disk* sda = Disk::create(staging, "/dev/sda", 1 * GiB);
+    sda->set_topology(Topology(0, 2 * MiB));
+    Msdos* msdos = to_msdos(sda->create_partition_table(PtType::MSDOS));
+    Partition* sda1 = msdos->create_partition("/dev/sda1", Region(2048, 2095104, 512), PartitionType::PRIMARY);
+
+    LvmVg* lvm_vg = LvmVg::create(staging, "test");
+    const LvmPv* lvm_pv = lvm_vg->add_lvm_pv(sda1);
+
+    BOOST_CHECK_EQUAL(lvm_pv->get_usable_size(), sda1->get_size() - 2 * MiB);
+}
