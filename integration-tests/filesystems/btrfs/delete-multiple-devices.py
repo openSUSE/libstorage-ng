d0508254c24626b3fe2acae843f8675bedcb8649
Arvin Schnell <aschnell@suse.de>
2019-04-11 14:51:02 +0200

    - added integration tests

---
+++ integration-tests/filesystems/btrfs/delete-multiple-devices.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# requirements: btrfs on /dev/sdc1 and /dev/sdd1
+
+
+from storage import *
+from storageitu import *
+
+
+set_logger(get_logfile_logger())
+
+environment = Environment(False)
+
+storage = Storage(environment)
+storage.probe()
+
+staging = storage.get_staging()
+
+print(staging)
+
+sdc1 = Partition.find_by_name(staging, "/dev/sdc1")
+sdc1.remove_descendants()
+
+print(staging)
+
+commit(storage)
+
