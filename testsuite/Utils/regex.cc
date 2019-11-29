c1f9dccdebe71cc55ebb3d8c3f1f2d7ced329f7a
Arvin Schnell <aschnell@suse.de>
2019-11-29 20:09:17 +0100

    - extended unit test (bsc#1135341)

--- testsuite/Utils/regex.cc
+++ testsuite/Utils/regex.cc
@@ -18,7 +18,9 @@ BOOST_AUTO_TEST_CASE(test_regex_escape1)
 
 /*
  * Check that for single char strings 1. the regex compilation with
- * the escaped string works and that 2. the string only matches itself.
+ * the escaped string works and that 2. the string only matches
+ * itself. The checks are also done when the escaped string is places
+ * inside rounded brackets.
  */
 BOOST_AUTO_TEST_CASE(test_regex_escape2)
 {
@@ -29,11 +31,18 @@ BOOST_AUTO_TEST_CASE(test_regex_escape2)
 
     for (const string& s1 : v)
     {
-	regex rx1;
+	string es1 = regex_escape(s1, regex::extended);
 
-	BOOST_CHECK_NO_THROW(rx1.assign(regex_escape(s1, regex::extended), regex::extended));
+	regex rx;
+
+	BOOST_CHECK_NO_THROW(rx.assign(es1, regex::extended));
 
 	for (const string& s2 : v)
-	    BOOST_CHECK_EQUAL(regex_match(s2, rx1), s1 == s2);
+	    BOOST_CHECK_EQUAL(regex_match(s2, rx), s1 == s2);
+
+	BOOST_CHECK_NO_THROW(rx.assign("(" + es1 + ")", regex::extended));
+
+	for (const string& s2 : v)
+	    BOOST_CHECK_EQUAL(regex_match(s2, rx), s1 == s2);
     }
 }
