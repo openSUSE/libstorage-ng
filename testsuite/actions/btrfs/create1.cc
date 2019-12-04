b2a42b9a0a7a6af33cdd06ac6e27292ddbbeaba7
Arvin Schnell <aschnell@suse.de>
2019-12-04 11:46:56 +0100

    - added testcases for growing btrfs

--- testsuite/actions/btrfs/create1.cc
+++ testsuite/actions/btrfs/create1.cc
@@ -17,6 +17,6 @@ BOOST_AUTO_TEST_CASE(actions)
 {
     set_logger(get_stdout_logger());
 
-    TsCmpActiongraph cmp("btrfs1", true);
+    TsCmpActiongraph cmp("create1", true);
     BOOST_CHECK_MESSAGE(cmp.ok(), cmp);
 }
