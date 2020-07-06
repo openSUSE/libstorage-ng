cc13b8411e5945a460bb58f796140913af106e54
Arvin Schnell <aschnell@suse.de>
2020-07-06 09:41:38 +0200

    - extended integration tests

--- integration-tests/pools/list.py
+++ integration-tests/pools/list.py
@@ -11,11 +11,13 @@ environment = Environment(True)
 storage = Storage(environment)
 storage.probe()
 
-storage.generate_pools(storage.get_probed())
+probed = storage.get_probed()
+
+storage.generate_pools(probed)
 
 for name in storage.get_pool_names():
     print(name)
 
     pool = storage.get_pool(name)
-    for device in pool.get_devices(storage.get_probed()):
+    for device in pool.get_devices(probed):
         print("    " + device.get_displayname())
--- integration-tests/pools/max-size.py
+++ integration-tests/pools/max-size.py
@@ -11,15 +11,18 @@ environment = Environment(True)
 storage = Storage(environment)
 storage.probe()
 
-storage.generate_pools(storage.get_probed())
+probed = storage.get_probed()
+
+storage.generate_pools(probed)
 
 pool = storage.get_pool("HDDs (512 B)")
 
-try:
-    max_size = pool.max_partition_size(storage.get_probed(), 2)
-except PoolOutOfSpace as exception:
-    print(exception.what())
-    exit(1)
+for n in range(1, len(pool.get_devices(probed)) + 1):
 
-print(max_size)
-print(byte_to_humanstring(max_size, False, 2, True))
+    try:
+        max_size = pool.max_partition_size(probed, n)
+    except PoolOutOfSpace as exception:
+        print(exception.what())
+        exit(1)
+
+    print(n, max_size, byte_to_humanstring(max_size, False, 2, True))
