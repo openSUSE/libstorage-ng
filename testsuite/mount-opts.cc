c05a557cdb5fe6883f3a8453046947f7c57d5571
Arvin Schnell <aschnell@suse.de>
2021-05-21 16:23:15 +0200

    - remove double mount options (see bsc#1186298)

--- testsuite/mount-opts.cc
+++ testsuite/mount-opts.cc
@@ -2,11 +2,13 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE libstorage
 
-#include <boost/test/unit_test.hpp>
 #include <iostream>
+#include <boost/algorithm/string.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include "storage/EtcFstab.h"
 #include "storage/Filesystems/FilesystemImpl.h"
+
 
 using namespace std;
 using namespace storage;
@@ -63,4 +65,41 @@ BOOST_AUTO_TEST_CASE(subvol_path_leading_slash)
 
     BOOST_CHECK(mount_opts.has_subvol(262, "home"));
     BOOST_CHECK(!mount_opts.has_subvol(262, "does-not-exist"));
+}
+
+
+BOOST_AUTO_TEST_CASE(test_ctor)
+{
+    BOOST_CHECK(MountOpts("").empty());
+    BOOST_CHECK(MountOpts(vector<string>({})).empty());
+
+    BOOST_CHECK(MountOpts("defaults").empty());
+    BOOST_CHECK(MountOpts(vector<string>({ "defaults" })).empty());
+
+    BOOST_CHECK_EQUAL(boost::join(MountOpts("noauto,user").get_opts(), ","), "noauto,user");
+    BOOST_CHECK_EQUAL(boost::join(MountOpts(vector<string>({ "noauto", "user" })).get_opts(), ","), "noauto,user");
+}
+
+
+BOOST_AUTO_TEST_CASE(test_remove)
+{
+    MountOpts mount_opts("ro,noauto,ro");
+
+    mount_opts.remove("ro");
+    BOOST_CHECK_EQUAL(boost::join(mount_opts.get_opts(), ","), "noauto");
+
+    mount_opts.remove("noauto");
+    BOOST_CHECK_EQUAL(boost::join(mount_opts.get_opts(), ","), "");
+}
+
+
+BOOST_AUTO_TEST_CASE(test_append)
+{
+    MountOpts mount_opts("ro");
+
+    mount_opts.append("noauto");
+    BOOST_CHECK_EQUAL(boost::join(mount_opts.get_opts(), ","), "ro,noauto");
+
+    mount_opts << "ro";
+    BOOST_CHECK_EQUAL(boost::join(mount_opts.get_opts(), ","), "ro,noauto,ro");
 }
