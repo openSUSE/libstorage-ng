7f4f5dbf0dbe028d4ecc1799c9d3f19acd1507ef
Arvin Schnell <aschnell@suse.de>
2023-01-17 12:47:18 +0100

    - added testcase

---
+++ testsuite/probe/swap1.cc
@@ -0,0 +1,43 @@
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE libstorage
+
+#include <boost/test/unit_test.hpp>
+
+#include "storage/Environment.h"
+#include "storage/Storage.h"
+#include "storage/Devicegraph.h"
+#include "storage/UsedFeatures.h"
+
+#include "testsuite/helpers/TsCmp.h"
+
+
+using namespace std;
+using namespace storage;
+
+/**
+ * One swap has path 'swap' in fstab, the other 'none'. See fstab(5).
+ */
+BOOST_AUTO_TEST_CASE(probe)
+{
+    set_logger(get_stdout_logger());
+
+    Environment environment(true, ProbeMode::READ_MOCKUP, TargetMode::DIRECT);
+    environment.set_mockup_filename("swap1-mockup.xml");
+
+    Storage storage(environment);
+    storage.probe();
+
+    const Devicegraph* probed = storage.get_probed();
+    probed->check();
+
+    Devicegraph* staging = storage.get_staging();
+    staging->load("swap1-devicegraph.xml");
+    staging->check();
+
+    TsCmpDevicegraph cmp(*probed, *staging);
+    BOOST_CHECK_MESSAGE(cmp.ok(), cmp);
+
+    BOOST_CHECK_EQUAL(required_features(probed), "btrfs swap");
+    BOOST_CHECK_EQUAL(suggested_features(probed), "btrfs swap");
+}
