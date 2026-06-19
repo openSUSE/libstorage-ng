1cad309b822b401cb1eea0cac24b52b8610edaa6
Arvin Schnell <aschnell@suse.de>
2026-06-19 13:26:29 +0200

    - fixed build on SLE 15

--- testsuite/Utils/usleep.cc
+++ testsuite/Utils/usleep.cc
@@ -6,6 +6,7 @@
 
 #include "storage/Utils/AppUtil.h"
 #include "storage/Utils/Stopwatch.h"
+#include "storage/Utils/LoggerImpl.h"
 
 
 using namespace std;
@@ -14,11 +15,13 @@ using namespace storage;
 
 BOOST_AUTO_TEST_CASE(usleep1)
 {
+    set_logger(get_stdout_logger());
+
     Stopwatch stopwatch;
 
     storage::usleep(10000);
 
-    cout << "stopwatch " << stopwatch << '\n';
+    y2mil("stopwatch " << stopwatch);
 
     BOOST_CHECK_GT(stopwatch.read(), 0.01);
 }
@@ -26,11 +29,13 @@ BOOST_AUTO_TEST_CASE(usleep1)
 
 BOOST_AUTO_TEST_CASE(usleep2)
 {
+    set_logger(get_stdout_logger());
+
     Stopwatch stopwatch;
 
     storage::usleep(1020000);
 
-    cout << "stopwatch " << stopwatch << '\n';
+    y2mil("stopwatch " << stopwatch);
 
     BOOST_CHECK_GT(stopwatch.read(), 1.02);
 }
